1e423134966ce145fbc28b988ee494679cffacb6
HyunwooSon <sonam@DESKTOP-986SC9M>
2020-10-10 01:28:22 +0900

    Add project files.

---
+++ CircuitImg/Controllers/ImageController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CircuitImg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ImageController> _logger;
+
+        public ImageController(ILogger<ImageController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
