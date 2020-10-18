e1e24910eed3bf313c98fc71e2778f1f47d70705
HyunwooSon <sonam@DESKTOP-986SC9M>
2020-10-18 11:38:42 +0900

    1013

--- CircuitImg/Controllers/SystemsController.cs
+++ CircuitImg/Controllers/SystemsController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Xamarin.Forms.PlatformConfiguration;
 
 namespace CircuitImg.Controllers
 {
