79167a732dd7048fe5a7b3e995a7e0558c15564f
HyunwooSon <sonam@DESKTOP-986SC9M>
2020-11-12 19:57:40 +0900

    commit

--- CircuitImg/Controllers/Controller.cs
+++ CircuitImg/Controllers/Controller.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,10 +13,21 @@ namespace CircuitImg.Controllers
     [ApiController]
     public class Controller : ControllerBase
     {
+        static SerialPort _serialPort = new SerialPort("COM4", 9600);
 
         [HttpGet]
-        public string Get()
+        public string Get(string interval)
         {
+            string _interval = "1000";
+
+            if (int.TryParse(interval, out int a))
+                _interval = interval;
+
+            if (!_serialPort.IsOpen)
+                _serialPort.Open();
+            var buffer = Encoding.UTF8.GetBytes(_interval);
+            _serialPort.Write(buffer,0,buffer.Length);
+            _serialPort.Close();
             return "Hello World";
         }
     }
--- CircuitImg/Controllers/SystemsController.cs
+++ CircuitImg/Controllers/SystemsController.cs
@@ -1,18 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace CircuitImg.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("test")]
     public class SystemsController : ControllerBase
     {
-        private void test()
+        static SerialPort _serialPort = new SerialPort("COM4",9600);
+
+        private readonly ILogger<SystemsController> _logger;
+
+        public SystemsController(ILogger<SystemsController> logger)
         {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        private void Get(string interval)
+        {
+            string _interval = "1000";
+
+            if (int.TryParse(interval, out int a))
+                _interval = interval;
+
+            _serialPort.Open();
+
+            _serialPort.Write(_interval);
         }
     }
 }
